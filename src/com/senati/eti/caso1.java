dd5031febc41d5b1230de484e0f2a148ccf8c1fe
Eduardo <Eduardo@Eduardo.lan>
2021-05-07 00:58:18 -0500

    Solucion Guia3

---
+++ src/com/senati/eti/caso1.java
@@ -0,0 +1,17 @@
+package com.senati.eti;
+
+public class caso1 {
+
+	public static void main(String[] args) {
+		// Estructuras Repetitivas
+		// Mientras - while
+		// Para     - For
+		int x = 1;
+		while (x <= 5) {
+			System.out.println("Bienvenido " + x);
+			x++;
+		}
+
+	}
+
+}
